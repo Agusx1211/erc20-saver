32beeda34e4b20d853548c5d34467d2c0796c9d2
Ignacio Mazzara <nachomazzara@gmail.com>
2020-03-02 12:48:56 -0300

    feat: add OpenSea projects

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -1,4 +1,5 @@
 export default {
   "0x8e5660b4ab70168b5a6feea0e0315cb49c8cd539": "Decentraland Marketplace",
-  "0xe479dfd9664c693b2e2992300930b00bfde08233": "Decentraland Bids"
+  "0xe479dfd9664c693b2e2992300930b00bfde08233": "Decentraland Bids",
+  "0xe5c783ee536cf5e63e792988335c4255169be4e1": "OpenSea Wyvern Token Transfer Proxy",
 }
